1fdc4c9a97755c800af9169eaa85bfc71aa03879
Travis Smith <smithtm90@gmail.com>
2016-06-04 16:40:59 -0700

    first version of Yahtzee, mostly css work

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready() {
+  //new game button resets the .val() of the corresponding <td>
+  //rules button opens a dialoge box with the rules for yahtzee in it
+  //roll button causes dice to 'animate' and stop on the randomly selected face
+  //selecting a category to score makes that category unplayable again during the same game
+  //categories can be played for 0 score if needed but will input appropriate score if dice fit the requirements for that category
+  //applying a score to a category will require checking of the face values and populating the correct table cell, with the total of all dice accepted by that category
+  //
+
+}
