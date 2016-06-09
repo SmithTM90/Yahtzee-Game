9db7f541e957df8e61d0393f1df13bf0cb951106
Travis Smith <smithtm90@gmail.com>
2016-06-09 01:56:59 -0700

    First live version of yahtzee game

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 var upperTotal = 0;
 var lowerTotal = 0;
 var diceCount = 0;
-
+//Player object holds values for the dice images, to be referenced in the roll button click handler
 var Player = {
   count: 0,
   dice1: {
@@ -65,88 +65,13 @@ var Player = {
     }
   }
 };
-
-var scoreBoard = {
-  aces: {
-
-  },
-  twos: {
-
-  },
-  threes: {
-
-  },
-  fours: {
-
-  },
-  fives: {
-
-  },
-  sixes: {
-
-  },
-  totalScore: {
-
-  },
-  bonus: {
-
-  },
-  newTotal: {
-
-  },
-  threeOAK: {
-    dice1: 0,
-    dice2: 0,
-    dice3: 0
-
-  },
-  fourOAK: {
-
-  },
-  fullHouse: {
-
-  },
-  smStr: {
-
-  },
-  lgStr: {
-
-  },
-  yahtzee: {
-    dice1: 0,
-    dice2: 0,
-    dice3: 0,
-    dice4: 0,
-    dice5: 0
-  },
-  chase: {
-
-  },
-  yahtzeeBonus: {
-
-  },
-  totalOfLower: {
-
-  },
-  totalOfUpper: {
-
-  },
-  grandTotal: {
-
-  }
-}
-
-var checkArray = [];
-
+//I'm only referencing this array in the roll functionality, but when I sub out the array.length + 1 for it's equivalent numerical value (, sometimes my rolls give me a 0 value.
 var diceArray = [{src: "img/1_dot.png" },
                 {src: "img/2_dots.png"},
                 {src: "img/3_dots.png"},
                 {src: "img/4_dots.png"},
                 {src: "img/5_dots.png"},
                 {src: "img/6_dots.png"}];
-
-var diceRoll;
-var randomDie = 0;
 
 $(document).ready(function() {
   //new game button resets the .val() of the corresponding <td>
@@ -157,28 +82,27 @@ $(document).ready(function() {
   //rules button opens a dialoge box with the rules for yahtzee in it
   $('#rules').on('click', function() {
     console.log("rules");
-  })
+  });
   //below function handles adding the elements of checkArray together for the purpose of returning a sum to be inputed to the score card
   function add(a,b) {
     return a + b;
   };
-
-  $('#cb1').on('click', function(){
-    Player.dice1.held=true;
+  //This handles the checkboxes for each dice, holding it so that it isn't rolled, could be more DRY.
+  $('#checkbox1').on('change', function(){
+    Player.dice1.held = $(this).prop("checked");
   });
-  $('#cb2').on('click', function(){
-    Player.dice2.held=true;
+  $('#checkbox2').on('change', function(){
+    Player.dice2.held = $(this).prop("checked");
   });
-  $('#cb3').on('click', function(){
-    Player.dice3.held=true;
+  $('#checkbox3').on('change', function(){
+    Player.dice3.held = $(this).prop("checked");
   });
-  $('#cb4').on('click', function(){
-    Player.dice4.held=true;
+  $('#checkbox4').on('change', function(){
+    Player.dice4.held = $(this).prop("checked");
   });
-  $('#cb5').on('click', function(){
-    Player.dice5.held=true;
+  $('#checkbox5').on('change', function(){
+    Player.dice5.held = $(this).prop("checked");
   });
-
   $('#rollbtn').on('click', function() {
     //after 3 rolls, choose a category, once chosen, reset player.count to 0 and all checkboxes to false
       if(Player.count === 3) {
@@ -201,9 +125,7 @@ $(document).ready(function() {
         }
         Player.count++;
       }
-      //This was a 3 person effort, but not very much from myself.
       for(var key in Player) {
-        console.log(Player[key].faceValue);
         if(key.indexOf('dice') !== -1){
           diceCount++
           $('#pic' + diceCount).attr('src', Player['dice' + diceCount].sides[Player[key].faceValue]);
@@ -211,8 +133,8 @@ $(document).ready(function() {
       }
       diceCount = 0;
   });
-
   $('#aces').on('click', function(){
+    console.log(document.getElementById('checkbox1'))
     var total = 0;
     if(Player.dice1.faceValue === 1){
       total = total + 1;
@@ -230,18 +152,14 @@ $(document).ready(function() {
       total = total + 1;
     }
     $('#aces').html(total);
-    for(var i = 0; i < scoreArray.length; i++){
-      if(scoreArray[i].innerHTML === "") {
-        return;
-      } else {
-        var arrayScore = scoreArray[i].innerHTML;
-        //console.log(arrayScore);
-        var scoreNumber = parseInt(arrayScore);
-        upperTotal += scoreNumber;
-      }
-    }
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   });
-
   $('#twos').on('click', function(){
     var total = 0;
     if(Player.dice1.faceValue === 2){
@@ -260,18 +178,14 @@ $(document).ready(function() {
       total = total + 2;
     }
     $('#twos').html(total);
-    for(var i = 0; i < scoreArray.length; i++){
-      if(scoreArray[i].innerHTML === "") {
-        return;
-      } else {
-        var arrayScore = scoreArray[i].innerHTML;
-        //console.log(arrayScore);
-        var scoreNumber = parseInt(arrayScore);
-        upperTotal += scoreNumber;
-      }
-    }
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   });
-
   $('#threes').on('click', function(){
     var total = 0;
     if(Player.dice1.faceValue === 3){
@@ -290,18 +204,14 @@ $(document).ready(function() {
       total = total + 3;
     }
     $('#threes').html(total);
-    for(var i = 0; i < scoreArray.length; i++){
-      if(scoreArray[i].innerHTML === "") {
-        return;
-      } else {
-        var arrayScore = scoreArray[i].innerHTML;
-        //console.log(arrayScore);
-        var scoreNumber = parseInt(arrayScore);
-        upperTotal += scoreNumber;
-      }
-    }
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   });
-
   $('#fours').on('click', function(){
     var total = 0;
     if(Player.dice1.faceValue === 4){
@@ -320,18 +230,14 @@ $(document).ready(function() {
       total = total + 4;
     }
     $('#fours').html(total);
-    for(var i = 0; i < scoreArray.length; i++){
-      if(scoreArray[i].innerHTML === "") {
-        return;
-      } else {
-        var arrayScore = scoreArray[i].innerHTML;
-        //console.log(arrayScore);
-        var scoreNumber = parseInt(arrayScore);
-        upperTotal += scoreNumber;
-      }
-    }
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   });
-
   $('#fives').on('click', function(){
     var total = 0;
     if(Player.dice1.faceValue === 5){
@@ -350,18 +256,14 @@ $(document).ready(function() {
       total = total + 5;
     }
     $('#fives').html(total);
-    for(var i = 0; i < scoreArray.length; i++){
-      if(scoreArray[i].innerHTML === "") {
-        return;
-      } else {
-        var arrayScore = scoreArray[i].innerHTML;
-        //console.log(arrayScore);
-        var scoreNumber = parseInt(arrayScore);
-        upperTotal += scoreNumber;
-      }
-    }
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   });
-
   $('#sixes').on('click', function(){
     var total = 0;
     if(Player.dice1.faceValue === 6){
@@ -380,18 +282,14 @@ $(document).ready(function() {
       total = total + 6;
     }
     $('#sixes').html(total);
-    for(var i = 0; i < scoreArray.length; i++){
-      if(scoreArray[i].innerHTML === "") {
-        return;
-      } else {
-        var arrayScore = scoreArray[i].innerHTML;
-        //console.log(arrayScore);
-        var scoreNumber = parseInt(arrayScore);
-        upperTotal += scoreNumber;
-      }
-    }
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   });
-
   $('#totalScore').on('click', function(){
     var scoreArray = $('#topTable .scoreCell');
     for(var i = 0; i < scoreArray.length; i++){
@@ -399,7 +297,6 @@ $(document).ready(function() {
         return;
       } else {
         var arrayScore = scoreArray[i].innerHTML;
-        //console.log(arrayScore);
         var scoreNumber = parseInt(arrayScore);
         upperTotal += scoreNumber;
       }
@@ -409,82 +306,109 @@ $(document).ready(function() {
     if (upperTotal > 63) {
       $('#bonus').html(35);
       $('#newTotal').html(upperTotal + 65);
-
     }
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   });
-
   $('#threeOAK').on('click', function(){
+    var threeArray = [];
     for (var key in Player) {
       var fValue = Player[key].faceValue;
       if(fValue) {
-        checkArray.push(fValue);
-        checkArray.sort();
+        threeArray.push(fValue);
+        threeArray.sort();
       }
     }
-    var length = checkArray.length;
-    var arrayString = checkArray.toString();
+    var length = threeArray.length;
+    var arrayString = threeArray.toString();
     console.log(arrayString);
     var arrayPattern = new RegExp("1,1,1|2,2,2|3,3,3|4,4,4|5,5,5|6,6,6");
     var patternCheck = arrayPattern.test(arrayString);
     console.log(length, patternCheck);
     if (patternCheck === true) {
       var splitArray = arrayString.split(",");
-      $('#threeOAK').html(checkArray.reduce(add, 0))
+      $('#threeOAK').html(threeArray.reduce(add, 0))
     } else {
       $('#threeOAK').html('0');
     }
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   });
-
   $('#fourOAK').on('click', function(){
+    var fourArray = [];
     for (var key in Player) {
       var fValue = Player[key].faceValue;
       if(fValue) {
-        checkArray.push(fValue);
-        checkArray.sort();
+        fourArray.push(fValue);
+        fourArray.sort();
       }
     }
-    var length = checkArray.length;
-    var arrayString = checkArray.toString();
+    var length = fourArray.length;
+    var arrayString = fourArray.toString();
     console.log(arrayString);
     var arrayPattern = new RegExp("1,1,1,1,|2,2,2,2|3,3,3,3|4,4,4,4|5,5,5,5|6,6,6,6");
     var patternCheck = arrayPattern.test(arrayString);
     console.log(length, patternCheck);
     if (patternCheck === true) {
       var splitArray = arrayString.split(",");
-      $('#fourOAK').html(checkArray.reduce(add, 0))
+      $('#fourOAK').html(fourArray.reduce(add, 0))
     } else {
       $('#fourOAK').html('0');
     }
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   });
-
   $('#fullHouse').on('click', function(){
+    var fullHouseArray = [];
     for (var key in Player) {
       var fValue = Player[key].faceValue;
       if(fValue) {
-        if(checkArray.indexOf(fValue) === -1) {
-          checkArray.push(fValue);
+        if(fullHouseArray.indexOf(fValue) === -1) {
+          fullHouseArray.push(fValue);
         }
       }
     }
-    var length = checkArray.length;
-    if (length < 2) {
+    var length = fullHouseArray.length;
+    if (length = 2) {
       $('#fullHouse').html('25');
     } else {
       $('#fullHouse').html('0');
     }
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   });
 //here is where I learned about Reg Ex!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
   $('#smStraight').on('click', function(){
+    var smallArray = [];
     for (var key in Player) {
       var fValue = Player[key].faceValue;
       if(fValue) {
-        checkArray.push(fValue);
-        checkArray.sort();
+        smallArray.push(fValue);
+        smallArray.sort();
       }
     }
-    var length = checkArray.length;
-    var arrayString = checkArray.toString();
+    var length = smallArray.length;
+    var arrayString = smallArray.toString();
     console.log(arrayString);
     var arrayPattern = new RegExp("1,2,3,4|2,3,4,5|3,4,5,6");
     var patternCheck = arrayPattern.test(arrayString);
@@ -494,18 +418,25 @@ $(document).ready(function() {
     } else {
       $('#smStraight').html('0');
     }
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   });
-
   $('#lgStraight').on('click', function(){
+    var largeArray = [];
     for (var key in Player) {
       var fValue = Player[key].faceValue;
       if(fValue) {
-        checkArray.push(fValue);
-        checkArray.sort();
+        largeArray.push(fValue);
+        largeArray.sort();
       }
     }
-    var length = checkArray.length;
-    var arrayString = checkArray.toString();
+    var length = largeArray.length;
+    var arrayString = largeArray.toString();
     console.log(arrayString);
     var arrayPattern = new RegExp("1,2,3,4,5|2,3,4,5,6");
     var patternCheck = arrayPattern.test(arrayString);
@@ -515,17 +446,24 @@ $(document).ready(function() {
     } else {
       $('#lgStraight').html('0');
     }
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   });
-
   $('#yahtzee').on('click', function(){
+    var yahtzeeArray = [];
     for (var key in Player) {
       var fValue = Player[key].faceValue;
       if(fValue) {
-        checkArray.push(fValue);
+        yahtzeeArray.push(fValue);
       }
     }
-    var length = checkArray.length;
-    var arrayString = checkArray.toString();
+    var length = yahtzeeArray.length;
+    var arrayString = yahtzeeArray.toString();
     console.log(arrayString);
     var arrayPattern = new RegExp("1,1,1,1,1|2,2,2,2,2|3,3,3,3,3|4,4,4,4,4|5,5,5,5,5|6,6,6,6,6");
     var patternCheck = arrayPattern.test(arrayString);
@@ -535,30 +473,34 @@ $(document).ready(function() {
     } else {
       $('#yahtzee').html('0');
     }
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   });
-  //I'm leaving this here to show my original check for a yahtzee, as some sort of relic to my past code and how I've re-factored it since then?
-
-  // $('#yahtzee').on('click', function(){
-  //   if(Player.dice1.faceValue === Player.dice2.faceValue && Player.dice1.faceValue === Player.dice3.faceValue && Player.dice1.faceValue === Player.dice4.faceValue && Player.dice1.faceValue === Player.dice5.faceValue) {
-  //     var total = Player.dice1.faceValue + Player.dice2.faceValue + Player.dice3.faceValue + Player.dice4.faceValue + Player.dice5.faceValue;
-  //     $('#yahtzee').html(total);
-  //   };
-  // });
-
   $('#chance').on('click', function(){
     total = 0;
+    var chanceArray = [];
     for (var key in Player) {
       var fValue = Player[key].faceValue;
       if(fValue) {
-        checkArray.push(fValue);
+        chanceArray.push(fValue);
       }
     }
-    console.log(checkArray);
-    for(var i=0; i < checkArray.length; i++) {
-      $('#chance').html(total += checkArray[i]);
+    for(var i=0; i < chanceArray.length; i++) {
+      $('#chance').html(total += chanceArray[i]);
     }
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   })
-
 $('#totalLower').on('click', function(){
     var scoreArray = $('#bottomTable .scoreCell');
     for(var i = 0; i < scoreArray.length; i++){
@@ -566,17 +508,18 @@ $('#totalLower').on('click', function(){
         return;
       } else {
         var arrayScore = scoreArray[i].innerHTML;
-        //console.log(arrayScore);
         var scoreNumber = parseInt(arrayScore);
         lowerTotal += scoreNumber;
       }
     }
     $('#totalLower').html(lowerTotal);
     $('#grandTotal').html(upperTotal + lowerTotal);
+    $('input[type="checkbox"]').prop("checked", false);
+    Player.dice1.held = false;
+    Player.dice2.held = false;
+    Player.dice3.held = false;
+    Player.dice4.held = false;
+    Player.dice5.held = false;
+    Player.count = 0;
   });
-
-  //selecting a category to score makes that category unplayable again during the same game
-  //categories can be played for 0 score if needed but will input appropriate score if dice fit the requirements for that category
-  //applying a score to a category will require checking of the face values and populating the correct table cell, with the total of all dice accepted by that category
-  //
 });
